0325816ffe72580297bf982bd8d5d4156a876e61
ChingizPro <chingizprog@gmail.com>
2022-01-26 07:14:45 +0500

    Create and add slider

---
+++ js/swiper.js
@@ -0,0 +1,18 @@
+(function(){
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        loop: true,
+        speed: 500,
+        
+        // Autoplay
+        autoplay: {
+          delay: 5000,
+        }, 
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }
+      });
+})();
