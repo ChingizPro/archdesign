002edc0b08f226a351d2d25c29b2d7beb3f3fea2
ChingizPro <chingizprog@gmail.com>
2022-02-03 01:33:08 +0500

    Create about page, add first section, style and make responsive

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,68 +1,75 @@
-(function(){
-    var elDropdown = $('.nav__dropdown');
-    var elDropdownToggler = $('.nav__lang');
-    var elDropdownList = $('.nav__list--langs');
+(function () {
+	var elDropdown = $(".nav__dropdown");
+	var elDropdownToggler = $(".nav__lang");
+	var elDropdownList = $(".nav__list--langs");
 
-    var hideDropdown = (evt) => {
-        if(!evt.target.classList.contains('tick') && !evt.target.classList.contains('langclick')){
-            elDropdownList.classList.remove('dropdown--show');
-            elDropdownToggler.classList.remove('active');
-            elDropdownToggler.removeEventListener('click', hideDropdownOnTogglerClick);
-            document.removeEventListener('click', hideDropdown);
-        }
-    }
-    
-    var hideDropdownOnTogglerClick = () => {
-        elDropdownList.classList.remove('dropdown--show');
-        elDropdownToggler.classList.remove('active');
-        elDropdownToggler.removeEventListener('click', hideDropdownOnTogglerClick);
-        document.removeEventListener('click', hideDropdown);
-    }
+	var hideDropdown = (evt) => {
+		if (
+			!evt.target.classList.contains("tick") &&
+			!evt.target.classList.contains("langclick")
+		) {
+			elDropdownList.classList.remove("dropdown--show");
+			elDropdownToggler.classList.remove("active");
+			elDropdownToggler.removeEventListener(
+				"click",
+				hideDropdownOnTogglerClick
+			);
+			document.removeEventListener("click", hideDropdown);
+		}
+	};
 
-    var showDropdown = () => {
-        elDropdownList.classList.add('dropdown--show');
-        elDropdownToggler.classList.add('active');
-        document.addEventListener('click', hideDropdown);
-        elDropdownToggler.addEventListener('click', hideDropdownOnTogglerClick);
-    }
+	var hideDropdownOnTogglerClick = () => {
+		elDropdownList.classList.remove("dropdown--show");
+		elDropdownToggler.classList.remove("active");
+		elDropdownToggler.removeEventListener("click", hideDropdownOnTogglerClick);
+		document.removeEventListener("click", hideDropdown);
+	};
 
-    elDropdownToggler.addEventListener('click', showDropdown);
+	var showDropdown = () => {
+		elDropdownList.classList.add("dropdown--show");
+		elDropdownToggler.classList.add("active");
+		document.addEventListener("click", hideDropdown);
+		elDropdownToggler.addEventListener("click", hideDropdownOnTogglerClick);
+	};
 
-    // * lang select
-    var setUzLang = () => {
-        elDropdownToggler.querySelector('.lang__img').src = 'img/uzb_flag_logo.png';
-        elDropdownToggler.querySelector('.lang__text').textContent = 'Uz';
-        localStorage.setItem('lang', 'uz');
-    }
+	elDropdownToggler.addEventListener("click", showDropdown);
 
-    var setRuLang = () => {
-        elDropdownToggler.querySelector('.lang__img').src = 'img/russia_flag_logo.png';
-        elDropdownToggler.querySelector('.lang__text').textContent = 'Ru';
-        localStorage.setItem('lang', 'ru');
-    }
+	// * lang select
+	var setUzLang = () => {
+		elDropdownToggler.querySelector(".lang__img").src = "img/uzb_flag_logo.png";
+		elDropdownToggler.querySelector(".lang__text").textContent = "Uz";
+		localStorage.setItem("lang", "uz");
+	};
 
-    if(localStorage.getItem('lang')){
-        if(localStorage.getItem('lang') === 'uz'){
-            setUzLang();
-        } else {
-            setRuLang();
-        }
-    }
+	var setRuLang = () => {
+		elDropdownToggler.querySelector(".lang__img").src =
+			"img/russia_flag_logo.png";
+		elDropdownToggler.querySelector(".lang__text").textContent = "Ru";
+		localStorage.setItem("lang", "ru");
+	};
 
-    var langSelect = (evt) => {
-        if(!evt.target.classList.contains('tick')){
-           return;
-        }
+	if (localStorage.getItem("lang")) {
+		if (localStorage.getItem("lang") === "uz") {
+			setUzLang();
+		} else {
+			setRuLang();
+		}
+	}
 
-        var lang = evt.target.dataset.id;
-        location.reload();
+	var langSelect = (evt) => {
+		if (!evt.target.classList.contains("tick")) {
+			return;
+		}
 
-        if(lang === 'uz'){
-            setUzLang();
-        } else {
-            setRuLang();
-        }
-    }
+		var lang = evt.target.dataset.id;
+		location.reload();
 
-    elDropdown.addEventListener('click', langSelect);
+		if (lang === "uz") {
+			setUzLang();
+		} else {
+			setRuLang();
+		}
+	};
+
+	elDropdown.addEventListener("click", langSelect);
 })();
